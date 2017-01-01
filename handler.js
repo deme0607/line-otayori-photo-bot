afe0ee0db15f7486588fd7ae2d3bf5e07b160e24
Naoki Shimizu <naoki.shimizu@mercari.com>
2017-01-02 02:39:45 +0900

    1st implementation

--- handler.js
+++ handler.js
@@ -1,16 +1,63 @@
 'use strict';
 
-module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+const MESSAGE_TYPE_IMAGE = 'image';
+const API_BASE_URL = 'https://api.line.me/v2/bot/message/';
 
-  callback(null, response);
+const superagent = require('superagent');
+const nodemailer = require('nodemailer');
+const yaml = require('js-yaml');
+const fs = require('fs');
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+const config = yaml.load(fs.readFileSync('./config.yml', 'utf8'));
+
+const sendMessage = (replyToken, message) => {
+  superagent.post(API_BASE_URL + '/reply')
+    .set('Content-type', 'application/json; charset=UTF-8')
+    .set('Authorization',  'Bearer ' + config.line.accessToken)
+    .send({
+      replyToken: replyToken,
+      messages: [{type: 'text', text: message}],
+    })
+    .catch(error => {
+      console.log(error);
+    });
+};
+
+module.exports.lineWebhook = (event, context, callback) => {
+  const body = JSON.parse(event.body);
+
+  body.events.forEach(data => {
+    if (data.message.type !== MESSAGE_TYPE_IMAGE) return;
+
+    const messageId = data.message.id;
+    const replyToken = data.replyToken;
+
+    superagent.get(API_BASE_URL + messageId + '/content')
+      .set('Authorization',  'Bearer ' + config.line.accessToken)
+      .then(response => {
+        const transporter = nodemailer.createTransport(
+          'smtps://' + config.gmail.username + '%40gmail.com:' + config.gmail.password + '@smtp.gmail.com'
+        );
+
+        const mailOptions = {
+          from: '"LINE Otayori Photo" <' + config.gmail.username + '@gmail.com>',
+          to: config.otayoriPhoto.email,
+          subject: '写真',
+          attachments: [{filename: 'image.jpg', content: response.body}],
+        };
+
+        transporter.sendMail(mailOptions)
+          .then(() => {
+            sendMessage(replyToken, '画像を送信しました');
+          })
+          .catch((error) => {
+            sendMessage(replyToken, error);
+          });
+      })
+      .catch((error) => {
+        sendMessage(replyToken, error);
+      });
+  });
+
+  callback(null, {statusCode: 200, body: JSON.stringify({})});
 };
